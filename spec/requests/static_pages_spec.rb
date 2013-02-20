a173ea3a765d349fba6c0e52a16a80d7f5565bcc
Olivier Zeller <o.zeller@gmail.com>
2013-02-20 13:26:05 +0100

    new test for start page with click_link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,5 +10,14 @@ describe "StaticPages" do
     it { should have_link('Sign up') }
     it { should have_link('Sign in') }
     it { should have_link('Sign up as guest') }
+
+    it "should have the right links on the layout" do
+      click_link "sign_up"
+      page.should have_selector('h3',    text: 'Sign up')
+      #click_link "Sign in"
+      #page.should # fill in
+      #click_link "Sign up as guest"
+      #page.should # fill in
+    end
   end
 end
