f5b806d5a4ae0cbaec352d6cb3968ee593204062
Olivier Zeller <o.zeller@gmail.com>
2013-03-27 12:41:49 +0100

    ditched the idea of using pusher and notification for now..

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -97,15 +97,30 @@ class MeetingsController < ApplicationController
     end
   end
 
+  def update_old_list
+    @old_invited = []
+    @old_invited_by = []
+
+    current_user.meetings.where(["created_at < ?", Time.now - 10.minutes]).each do |m|
+      others = m.handshakes.where("user_id != ?", current_user.id)
+      others.where(validated: false).each do |h|
+        @old_invited << User.find_by_id(h.user_id)
+      end
+
+      m.handshakes.where(user_id: current_user.id, validated: false).each do |h|
+        @old_invited_by << { users: m.users.where("user_id != ?", current_user.id), 
+                             handshake_id: h.id }
+      end
+    end
+  end
+
   def update_list
     # 0.03 miles = 48.28032 meters
     nearbys = current_user.nearbys(0.03).where(location_time: (Time.now - 10.minutes)..Time.now)
     @around_me = nearbys
     @invited = []
     @invited_by = []
-    @catching_up_with = []
-    @old_invited = []
-    @old_invited_by = []
+    @catching_up_with = []   
 
     nearbys.each do |p|
       p.meetings.where(created_at: (Time.now - 10.minutes)..Time.now).each do |m|
@@ -117,7 +132,7 @@ class MeetingsController < ApplicationController
       end
     end
 
-    current_user.meetings.each do |m|
+    current_user.meetings.where(created_at: (Time.now - 10.minutes)..Time.now).each do |m|
       others = m.handshakes.where("user_id != ?", current_user.id)
       others.where(validated: false).each do |h|
         @invited << User.find_by_id(h.user_id)
@@ -129,8 +144,8 @@ class MeetingsController < ApplicationController
       end
     end
 
-    # not working for invited_by since the array has different elements
-    @around_me = nearbys - @catching_up_with - @invited - @invited_by
+    # for invited_by: maps the users and gets the first one if its a group meeting
+    @around_me = nearbys - @catching_up_with - @invited - @invited_by.map{ |e| e[:users][0] }
 
   end
 
